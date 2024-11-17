6f60b08910ac4fd2199ae5bb7a7e6c5c7cc83288
Suryansh Srivastava <suryanshhbtu@gmail.com>
2024-11-18 00:21:39 +0530

    Signup DTO

---
+++ src/main/java/com/springboot/myjwt/dto/RegisterUserDTO.java
@@ -0,0 +1,33 @@
+package com.springboot.myjwt.dto;
+
+public class RegisterUserDTO  {
+    private String email;
+
+    private String password;
+
+    private String userName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
